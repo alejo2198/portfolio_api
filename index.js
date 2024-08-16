2ec0c794b5e9af6d70278863f3a00117478a628c
Alejandro Castro <alejo199821@gmail.com>
2024-08-16 19:04:20 -0400

    update cors with production url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const db = require("./database/db");
 
 // Enable CORS for all routes
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'https://portfolio-b-two.vercel.app/');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
