47ef9731cb218f0909fbb1c885e78b1cb76b63f3
Alejandro Castro <alejo199821@gmail.com>
2024-08-16 12:31:16 -0400

    add cors for development

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const port = process.env.PORT || "8888"
 
 const db = require("./database/db");
 
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
